42d1283a0095001d142aa3c379af16f8c665e5a6
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-13 12:17:15 +0100

    Switch on step 21

--- lib/app.rb
+++ lib/app.rb
@@ -17,9 +17,21 @@ class Battle < Sinatra::Base
   get '/play' do
     @p1_name = session[:p1_name]
     @p2_name = session[:p2_name]
-    @p1_hit_points = 100
-    @p2_hit_points = 100
+    session[:p1_hp] = 100
+    session[:p2_hp] = 100
+    @turns = 0
     erb(:play)
+    puts "this code runs"
+    p session[:p2_hp]
+  end
+
+  post '/attack' do
+    # session[:p2_hp] -= 10
+    redirect '/finished_attack'
+  end
+
+  get '/finished_attack' do
+    erb(:attack)
   end
 
   # start the server if ruby file executed directly
